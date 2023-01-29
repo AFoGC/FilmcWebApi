2697d1f3a66d9f0317b2b4d87d1613188e4f7bd7
AFoGC <sirkoandrew84@gmail.com>
2023-01-29 11:27:29 +0200

    1

---
+++ FilmcWebApi/Controllers/UpdatesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmcWebApi.Controllers
+{
+    [Route("FilmcApi/[controller]")]
+    [ApiController]
+    public class UpdatesController : Controller
+    {
+        [HttpGet]
+        public byte[] GetLastFile()
+        {
+            return null;
+        }
+    }
+}
